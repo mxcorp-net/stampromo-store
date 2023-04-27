c9759feb8c0a919b905a0f87c445732036423780
Alfonso Rodríguez <alfonsor.nolasco@gmal.com>
2023-04-26 21:45:05 -0600

    Modules:  - Dashboard  - Products  - Shopping Cart  - Wishlist  - Info (Need more details)
     Components
     - Carousel

---
+++ src/app/_modules/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { CarouselComponent } from '../../_components/carousel/carousel.component';
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+//Modules
+import { DashboardComponent } from './_modules/dashboard/dashboard.component';
+import { ProductComponent } from './_modules/product/product.component';
+import { InfoComponent } from './_modules/info/info.component';
+import { CartShoppingComponent } from './_modules/cart-shopping/cart-shopping.component';
+import { WishlistComponent } from './_modules/wishlist/wishlist.component';
+
+
+//Components
+import {CarouselComponent} from "./_components/carousel/carousel.component";
+
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: DashboardComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'cart', component: CartShoppingComponent },
+  { path: 'wishlist', component: WishlistComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CarouselComponent,
+    ProductComponent,
+    InfoComponent,
+    CartShoppingComponent,
+    WishlistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
